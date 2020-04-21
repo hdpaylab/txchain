01a2bd422e70da13ab5b5cf35c8b11ddf934512b
aiadmin <tjsejrwnd@naver.com>
2020-04-21 09:38:35 +0900

    simple serialize

--- test/serialize_ex.cpp
+++ test/serialize_ex.cpp
@@ -1,76 +1,54 @@
-#include <boost\serialization\serialization.hpp>
-#include <boost\serialization\string.hpp>
-#include <boost\archive\binary_oarchive.hpp>
-#include <boost\archive\binary_iarchive.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <iostream>
-#include <fstream>
-#include <string>
+typedef struct A {
+	int a;
+	char *str;
+} test_struct_t;
 
-using namespace std;
+char *serialize(test_struct_t t) {
+	int str_len = strlen(t.str);
 
-class UserAccount
-{
-	private:
-		string name;
-		string id;
-		string pw;
-		int age;
+	int size = 2 * sizeof(int) + str_len;
+	char *buf = (char*)malloc(sizeof(char) * (size+1));
 
-		friend class boost::serialization::access; //직렬화해주는분께서 접근 할 수 있도록 friend선언
-		template<class Archive>
-			void serialize(Archive& ar, const unsigned int version)
-			{
-				ar& name;
-				ar& id;
-				ar& pw;
-				ar& age;
-			}
-	public:
-		UserAccount(string _name, string _id, string _pw, int _age)
-		{
-			name = _name;
-			id = _id;
-			pw = _pw;
-			age = _age;
-		}
-		UserAccount(){}
-		void Print(void)
-		{
-			cout<<"name:"<<name<<endl;
-			cout<<"id:"<<id<<endl;
-			cout<<"pw:"<<pw<<endl;
-			cout<<"age:"<<age<<endl;
-		}
-};
+	memcpy(buf, &t.a, sizeof(int));
+	memcpy(buf + sizeof(int), &str_len, sizeof(int));
+	memcpy(buf + sizeof(int) * 2, t.str, str_len);
+	buf[size] = '\0';
 
-int main(void)
-{
-	string name, id, pw;
-	char pointer[64];
-	int age;
-	cout<<"input name:";
-	cin>>name;
-	cout<<"input id:";
-	cin>>id;
-	cout<<"input pw:";
-	cin>>pw;
-	cout<<"input age:";
-	cin>>age;
-	UserAccount account(name,id,pw,age);
+	return buf;
+}
 
-	ofstream out; //쓰기 스트림 생성
-	out.open("serialization.test",ios_base::binary); //바이너리 모드로 파일을 열었습니다.
-	boost::archive::binary_oarchive out_archive(out); //연 스트림을 넘겨주어서 직렬화객체 초기화
-	out_archive<<account; //쓰기
-	out.close(); //닫기
+test_struct_t deserialize(char *buf) {
+	test_struct_t t;
 
-	ifstream in; //읽기 스트림 생성
-	UserAccount read_account; //받을 객체 생성
-	in.open("serialization.test",ios_base::binary); //바이너리모드로 파일을 열었습니다.
-	boost::archive::binary_iarchive in_archive(in); //연 스트림을 넘겨주어서 직렬화객체 초기화
-	in_archive>>read_account; //읽기
-	read_account.Print(); //닫기
+	memcpy(&t.a, buf, sizeof(int));
+
+	int str_len;
+	memcpy(&str_len, buf+sizeof(int), sizeof(int));
+
+	t.str = (char*)malloc(sizeof(char) * (str_len+1));
+	memcpy(t.str, buf+2*sizeof(int), str_len);
+	t.str[str_len] = '\0';
+
+	return t;
+}
+
+int main() {
+	char str[15] = "Hello, world!";
+
+	test_struct_t t;
+	t.a = 123;
+	t.str = (char*)malloc(strlen(str) + 1);
+	strcpy(t.str, str);
+	printf("original values: %d %s\n", t.a, t.str);
+
+	char *buf = serialize(t);
+	printf("buf[%s]\n", buf);
+	test_struct_t new_t = deserialize(buf);
+	printf("new values: %d %s\n", new_t.a, new_t.str);
 
 	return 0;
 }
